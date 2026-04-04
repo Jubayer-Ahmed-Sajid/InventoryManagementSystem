5108bc2ebb8676e60eff35f5b53894a2f465655c
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2026-04-04 14:27:15 +0600

    chore: initialize ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InventoryMangementSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
