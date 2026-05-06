f11aa4726d533de1b0f07acdb8786ded6bc4e5ab
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2026-05-06 23:19:17 +0600

    fix: format MySQL connection string and enable developer exception page

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("InventoryManagementSystem"),ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("InventoryManagementSystem"))));
+    options.UseMySql(builder.Configuration.GetConnectionString("InventoryManagementSystem"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("InventoryManagementSystem"))));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ProductServices>();
 
@@ -28,6 +28,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Product/Error");
+    app.UseDeveloperExceptionPage();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
